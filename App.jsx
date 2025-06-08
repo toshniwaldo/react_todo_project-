a95b2baf70cd0a9f37aced873aeb103944975a8f
Dhruv Toshniwal <134626880+toshniwaldo@users.noreply.github.com>
2025-06-08 06:36:46 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import React from 'react'; 
+import React from 'react';
 
 let count=0;
 function App() {
